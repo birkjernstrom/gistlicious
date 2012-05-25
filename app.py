bff9c489c63c5f5a7589f53db199fd1fa40d9bd3
William Tisäter <william@defunct.cc>
2012-05-25 22:46:29 +0200

    Add top list and upvote function.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+from giststorage import GistStorage
+from local import REDIS_PASSWORD
+
 app = Flask(__name__)
 app.debug = True
 
@@ -8,7 +11,19 @@ app.debug = True
 def index():
     return render_template('index.html')
 
+@app.route('/upvote', methods=['POST'])
+def upvote():
+    return '%s' % app.storage.upvote(int(request.form['gist_id']))
+
+@app.route('/toplist')
+def toplist():
+    toplist = app.storage.get_rank_list()
+    return render_template('toplist.html', toplist=toplist)
+
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
+    REDIS_HOST = 'scat.redistogo.com'
+    REDIS_PORT = 9176
+
+    app.storage = GistStorage(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
--- giststorage.py
+++ giststorage.py
@@ -1,29 +1,20 @@
 from redis import StrictRedis
-from local import REDIS_PASSWORD
 
-REDIS_HOST = 'scat.redistogo.com'
-REDIS_PORT = 9176
-
-
-class GistStorage(StrictRedis):
+class GistStorage(object):
     SUPER_KEY = 'testar'
 
-    def save_winner(self, gist_id):
-        return client.zincrby(self.SUPER_KEY, gist_id, 1)
+    def __init__(self, host, port, password):
+        self.client = StrictRedis(host=host, port=port, password=password)
 
-    def save_loser(self, gist_id):
-        return client.zincrby(self.SUPER_KEY, gist_id, 0)
+    def upvote(self, gist_id):
+        return self.client.zincrby(self.SUPER_KEY, gist_id, 1)
 
     def get_rank(self, gist_id):
-        return client.zrevrank(self.SUPER_KEY, gist_id)
+        return self.client.zrevrank(self.SUPER_KEY, gist_id)
 
     def get_points(self, gist_id):
-        return client.zscore(self.SUPER_KEY, gist_id)
+        return self.client.zscore(self.SUPER_KEY, gist_id)
 
     def get_rank_list(self, offset=0, num=-1):
-        return client.zrevrange(self.SUPER_KEY, offset, num, withscores=True, score_cast_func=float)
+        return self.client.zrevrange(self.SUPER_KEY, offset, num, withscores=True, score_cast_func=float)
 
-
-# client = GistStorage(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
-# client.save_winner(12346)
-# client.save_loser(12347)
