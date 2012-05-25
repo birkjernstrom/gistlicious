b5a27a05f75bb0ff9528df05ef9c6b42ae7a6767
siavashg <siavash@tictail.com>
2012-05-25 21:36:03 +0200

    Adding template structure

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+        return render_template('index.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
